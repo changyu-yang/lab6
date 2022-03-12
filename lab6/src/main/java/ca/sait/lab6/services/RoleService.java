305182c29f20a1aa37648693a47fa77f59e5b2af
changyu-yang <97761907+changyu-yang@users.noreply.github.com>
2022-03-12 10:30:22 +0900

    Add files via upload

---
+++ lab6/src/main/java/ca/sait/lab6/services/RoleService.java
@@ -0,0 +1,16 @@
+package ca.sait.lab6.services;
+
+import ca.sait.lab6.dataaccess.RoleDB;
+import ca.sait.lab6.dataaccess.UserDB;
+import ca.sait.lab6.models.Role;
+import ca.sait.lab6.models.User;
+import java.util.List;
+
+public class RoleService {
+    private RoleDB roleDB = new RoleDB();
+    
+    public List<Role> getAll() throws Exception {
+        List<Role> roles = roleDB.getAll();
+        return roles;
+    }
+}
